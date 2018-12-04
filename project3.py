3ac0b3a22d221192fc0d85c289a3553e244960db
Timothy Chan <chantg@uci.edu>
2018-12-04 05:29:28 -0800

    Returned to previous tokenizer

--- project3.py
+++ project3.py
@@ -33,27 +33,33 @@ def tokenize(file):
 		i=0
 		for item in fileList:
 			k +=1
-			with open(os.path.dirname(os.path.abspath(inspect.stack()[0][1]))+ dirName[1:] + "\\" + item) as f:
+			with open(dirName + "\\" + item, encoding = "utf8") as f:
 					#iterate through every line in the file
-				soup = BeautifulSoup(open(os.path.dirname(os.path.abspath(inspect.stack()[0][1]))+ dirName[1:] + "\\" + item , encoding="utf8"), "html.parser")
-				text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
-				text_tokens = [t for t in text_tokens if not re.match('[' + string.punctuation + ']+', t)]
-				for word in text_tokens:
-					if(word.lower() in tokens):
-						tokens[word.lower()][0] +=1;
-						if (fileID[len(fileID)-1] +"/" + str(i)) not in tokens[word.lower()][1]:
-							tokens[word.lower()][1][str(fileID[len(fileID)-1]) +"/"+ str(i)] = 1
-						else:
-							tokens[word.lower()][1][str(fileID[len(fileID)-1]) +"/"+str(i)] += 1
-					else:
-						doc_dict = {str(fileID[len(fileID)-1]) +"/"+str(i): 1}
-						tokens[word.lower()] = [1, doc_dict]
-				
-				i +=1
-			#if k > 3: #limits first 20 docs for testing, remove when submitting
+					try:
+						for line in f:
+						
+							words = re.split(r"[0-9\W_]+", line)
+					#iterate through every word on the line
+							for word in words:
+								if(word != ""):
+								#if it exists increment the pointer.
+									if(word.lower() in tokens):
+										tokens[word.lower()][0] +=1;
+										if (fileID[len(fileID)-1] +"/" + str(i)) not in tokens[word.lower()][1]:
+											tokens[word.lower()][1][str(fileID[len(fileID)-1]) +"/"+ str(i)] = 1
+										else:
+											tokens[word.lower()][1][str(fileID[len(fileID)-1]) +"/"+str(i)] += 1
+									else:
+										doc_dict = {str(fileID[len(fileID)-1]) +"/"+str(i): 1}
+										tokens[word.lower()] = [1, doc_dict]
+					except:
+						print(line)
+					i +=1
+			#if k > 1: #limits first 20 docs for testing, remove when submitting
 	for term in tokens:
 		for doc_id in tokens[term][1]:
 			tokens[term][1][str(doc_id)] = tfidf(tokens[term][1][doc_id], len(tokens[term][1]), k)
+	print("tokens created")
 	return tokens
 
 
@@ -67,7 +73,7 @@ try:
     #the lambda key in the sorted method call puts priority in sorting by the
     #most frequent words (highest value) and then alphabetically
 	client = pymongo.MongoClient("mongodb+srv://timothyChan:Unrealgamer%5F1@cs121-54zoj.mongodb.net/test?retryWrites=true")
-	db = client['NLTK'] 
+	db = client['CS121'] 
 	
 	web = db.Webpages
 	
